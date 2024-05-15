998451aef58d8198a79504dbd68ee85260e8007d
Gerhard Reinerth <g.reinerth@tum.de>
2024-05-15 14:21:10 +0000

    unit-test: SMDMD unit tests increased

--- tests/test_smdmd.py
+++ tests/test_smdmd.py
@@ -2,14 +2,14 @@
 
 import numpy as np
 
-from pydmd.smdmd import SMDMD
+from pydmd.smdmd import BOUND, SMDMD
 from pydmd.varprodmd import varprodmd_predict
 
 from .test_varprodmd import signal
 
 
 def test_smdmd_unconstrained_dense() -> None:
-    """Test DMD (utility)"""
+    """Test SMDMD reconstruction pure reconstruction capability"""
     time = np.linspace(0, 4 * np.pi, 100)
     x_loc = np.linspace(-10, 10, 1024)
     z = signal(*np.meshgrid(x_loc, time)).T
@@ -21,3 +21,68 @@ def test_smdmd_unconstrained_dense() -> None:
     omegas = np.log(dmd.eigs) / dmd.dmd_time["dt"]
     rec = varprodmd_predict(dmd.modes, omegas, dmd.amplitudes, time)
     assert np.linalg.norm(z - rec, axis=0).mean() < 1e-9
+
+
+def test_smdmd_unconstrained_sparse() -> None:
+    """Test SMDMD"""
+    time = np.linspace(0, 4 * np.pi, 100)
+    x_loc = np.linspace(-10, 10, 1024)
+    z = signal(*np.meshgrid(x_loc, time)).T
+    dmd = SMDMD(alpha=10.0, beta=0.001, qp_max_iter=10, prox_operator="prox_l1")
+
+    # update dmd time
+    dmd.dmd_time = {"dt": time[1] - time[0], "t0": time[0], "tend": time[-1]}
+    dmd.fit(z)
+    omegas = np.log(dmd.eigs) / dmd.dmd_time["dt"]
+    rec = varprodmd_predict(dmd.modes, omegas, dmd.amplitudes, time)
+    msk = (dmd.modes.real == 0) & (dmd.modes.imag == 0)
+
+    # assert that modes are indeed sparse
+    assert 0 < np.sum(msk) < np.prod(dmd.modes.shape)
+
+    # Test reconstruction capability, worse performance is assumed
+    assert np.linalg.norm(z - rec, axis=0).mean() < 0.05
+
+
+def test_smdmd_synthetic_signal_constrained() -> None:
+    """Test synthetic sparse (mode) signal"""
+
+    modes = np.zeros((1024, 4), dtype=complex)
+    modes.real = np.random.binomial(
+        1, 0.5, size=tuple(modes.shape)
+    ) * np.random.normal(size=tuple(modes.shape))
+    modes.imag = np.random.binomial(
+        1, 0.5, size=tuple(modes.shape)
+    ) * np.random.normal(size=tuple(modes.shape))
+
+    rb = BOUND(modes.real.min(), modes.real.max())
+    ib = BOUND(modes.imag.min(), modes.imag.max())
+
+    amps = np.linalg.norm(modes, axis=0)
+    modes /= amps[None]
+
+    omegas = np.zeros((modes.shape[-1],), dtype=complex)
+    omegas.real = np.random.normal(size=(modes.shape[-1],))
+    omegas.imag = np.random.normal(size=(modes.shape[-1],))
+
+    time = np.linspace(0.0, 1.0, 256)
+    synthethic_signal = varprodmd_predict(modes, omegas, amps, time)
+
+    dmd = SMDMD(alpha=10.0, beta=1e-3, rb=rb, ib=ib)
+    dmd.dmd_time = {"dt": time[1] - time[0], "t0": time[0], "tend": time[-1]}
+    dmd.fit(synthethic_signal)
+
+    scaled_modes = dmd.modes * dmd.amplitudes[None]
+    assert np.sum(scaled_modes.real < rb.lower) == 0
+    assert np.sum(scaled_modes.real > rb.upper) == 0
+    assert np.sum(scaled_modes.imag < ib.lower) == 0
+    assert np.sum(scaled_modes.imag > ib.upper) == 0
+
+    assert np.array_equal(dmd.dmd_timesteps, time)
+    rec = varprodmd_predict(
+        dmd.modes,
+        np.log(dmd.eigs) / dmd.dmd_time["dt"],
+        dmd.amplitudes,
+        dmd.dmd_timesteps,
+    )
+    assert np.linalg.norm(synthethic_signal - rec, axis=0).mean() < 1.0
